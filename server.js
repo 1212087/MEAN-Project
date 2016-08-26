be8a7781e076a5afaaaf77b83649da5e28d0ab1e
1212087 <1212087@student.hcmus.edu.vn>
2016-08-26 17:10:55 +0700

    Update server.js
    Fix mongodb bugs

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local'),
 	Strategy;
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
 
 // database
 var mongoose = require('mongoose');
